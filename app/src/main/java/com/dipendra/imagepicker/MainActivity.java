ce5f0c61101935274089d8ce5a354fb92c787f4c
Dipendramehra <dipendramehra2002@outlook.com>
2022-01-31 16:18:36 +0530

    Initial commit

---
+++ app/src/main/java/com/dipendra/imagepicker/MainActivity.java
@@ -0,0 +1,71 @@
+package com.dipendra.imagepicker;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class MainActivity extends AppCompatActivity {
+    //Declaring the widgets/ui components
+ImageView imageView;
+Button camera,gallery;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        camera=findViewById(R.id.btn_camera);
+     gallery=findViewById(R.id.btn_gallery);
+     imageView=findViewById(R.id.imageView);
+
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.CAMERA)==
+                    PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 0);
+        }
+
+    }
+
+//
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 0:
+                if (resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    imageView.setImageBitmap(imageBitmap);
+                }
+                break;
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = data.getData();
+                    imageView.setImageURI(selectedImage);
+                }
+                break;
+        }
+    }
+
+    public void pick_from_camera(View view) {
+        Intent takepicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takepicture,0);
+    }
+
+    public void pick_from_gallery(View view) {
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto, 1);
+    }
+}
